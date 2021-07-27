82a7505efc7f5311638ae81c66dae06adb8ecc50
Zubeyde-sayin-kutlug <szubeyde@hotmail.com>
2021-07-27 21:25:35 +0200

    File structure edited.

---
+++ src/components/pages/Dairy.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Dairy extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Dairy</h3>
+      </div>
+    );
+  }
+}
+
+export default Dairy;
---
+++ src/components/pages/Events.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class events extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Events</h3>
+      </div>
+    );
+  }
+}
+
+export default events;
