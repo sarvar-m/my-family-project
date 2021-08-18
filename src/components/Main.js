97129647d4c892678b8ca9eea9e975a51f0c50b0
Zubeyde-sayin-kutlug <szubeyde@hotmail.com>
2021-08-18 18:37:35 +0200

    Added routing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import MainNavbar from "./MainNavBar";
 import MainBody from "./MainBody";
 import "./NavBar.css";
 import MainFooter from "./MainFooter";
+import "./MainFooter.css";
 
 function Main() {
   return (
     <Router>
-      <div>
-        <MainNavbar />
-        <div className="pages">
-          <MainBody />
-          <MainFooter />
-        </div>
+      <div className="pages">
+        <MainBody />
+        <MainFooter />
       </div>
     </Router>
   );
