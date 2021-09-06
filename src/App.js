db68b5bdb5c5af64551337f4d5d4d3fc705a1e5e
Sarvar M <sarvar.m149@gmail.com>
2021-09-06 19:36:31 +0700

    create Sign Up page and set its routing

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Events from "./components/pages/Events";
 import LogOut from "./components/pages/LogOut";
 import Settings from "./components/pages/Settings";
 import Invite from "./components/pages/Invite";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/logout" component={LogOut} />
         <Route path="/settings" component={Settings} />
         <Route path="/invite" component={Invite} />
+        <Route path="/signup" component={SignUp} />
       </Switch>
     </Router>
   );
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+
+const SignUp = () => {
+  const [values, setValues] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    buttonText: "Submit",
+    confirmPasswordError: "",
+  });
+
+  const {
+    name,
+    surname,
+    email,
+    password,
+    confirmPassword,
+    confirmPasswordError,
+    buttonText,
+  } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const signupForm = () => (
+    <form>
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Name
+        </label>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control mb-3"
+          value={name}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Surname
+        </label>
+        <input
+          onChange={handleChange("surname")}
+          type="text"
+          className="form-control mb-3"
+          value={surname}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Email
+        </label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control mb-3"
+          value={email}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Password
+        </label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control mb-3"
+          value={password}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Confirm Password
+        </label>
+        <input
+          onChange={handleChange("confirmPassword")}
+          type="password"
+          className="form-control mb-3"
+          value={confirmPassword}
+        />
+      </div>
+      <div>
+        <button className="btn btn-primary" onClick={handleSubmit}>
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  );
+
+  return (
+    <div className="col-md-6 offset-md-3">
+      <h1 className="p-5 text-center my-4">Sign Up</h1>
+      {signupForm()}
+      <br />
+      <Link className="btn btn-sm btn-outline-danger">Forgot Password</Link>
+      <br />
+      <br />
+      <p>
+        If you already have registered, please <Link>Log In</Link> here
+      </p>
+    </div>
+  );
+};
+
+export default SignUp;
