015f02fcf2684df42d7331d6a8f1f1ec5beb2645
Sarvar M <sarvar.m149@gmail.com>
2021-09-25 23:27:49 +0700

    set user model, connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected!"))
+  .catch((err) => console.log(err.message));
+
 // import routes
 const authRoutes = require("./routes/auth.routes");
+
+// app middleware
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 // middleware
 app.use("/api", authRoutes);
